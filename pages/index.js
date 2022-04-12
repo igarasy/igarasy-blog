a3d4d318838d191da47681d79eb3a2b717506202
igarasy <igarasshi.lucass@gmail.com>
2022-04-11 22:16:55 -0300

    Add PostCard Component

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home({ posts }) {
       <div className="grid grid-cols-1 gap-12 lg:grid-cols-12">
         <div className="col-span-1 lg:col-span-8">
           {posts.map((post, index) => (
-            <PostCard post={post} key={post.title} />
+            <PostCard post={post.node} key={post.title} />
           ))}
         </div>
         <div className="col-span-1 lg:col-span-4">
