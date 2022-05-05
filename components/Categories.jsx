dfa02560fedea6b21edad15e8b7b2c1852dfd3cb
igarasy <igarasshi.lucass@gmail.com>
2022-05-04 22:27:04 -0300

    Implement the Categories component

--- components/Categories.jsx
+++ components/Categories.jsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import Link from 'next/Link';
+
+import { getCategories } from '../services';
 
 const Categories = () => {
-  return <div>Categories</div>;
+  const [categories, setCategories] = React.useState([]);
+
+  React.useEffect(() => {
+    getCategories().then((newCategories) => setCategories(newCategories));
+  }, []);
+
+  return (
+    <div className="mb-8 rounded-lg bg-white p-8 pb-12 shadow-lg">
+      <h3 className="mb-8 border-b pb-4 text-xl font-semibold">Categories</h3>
+      {categories.map((category) => (
+        <Link key={category.slug} href={`/category/${category.slug}`}>
+          <span className="mb-3 block cursor-pointer pb-3">
+            {category.name}
+          </span>
+        </Link>
+      ))}
+    </div>
+  );
 };
 
 export default Categories;
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,12 +1,14 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import Link from 'next/link';
-
-const categories = [
-  { name: 'React', slug: 'react' },
-  { name: 'Web Development', slug: 'web-dev' }
-];
+import { getCategories } from '../services';
 
 const Header = () => {
+  const [categories, setCategories] = React.useState([]);
+
+  React.useEffect(() => {
+    getCategories().then((newCategories) => setCategories(newCategories));
+  }, []);
+
   return (
     <div className="container mx-auto mb-8 px-10">
       <div className="inline-block w-full border-b border-blue-400 py-8">
