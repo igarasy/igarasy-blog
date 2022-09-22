33cb658380d1f5bba88fb3b9c9cbdb1700e98e06
igarasy <lucas.igarashi@caju.com.br>
2022-09-21 22:21:40 -0300

    feature: create author component

--- components/Author.jsx
+++ components/Author.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import Image from 'next/image';
 
-const Author = () => {
+const Author = ({ author }) => {
   return (
-    <div>
-      <h1>Author</h1>
+    <div className="relative mt-20 mb-8 rounded-lg bg-black bg-opacity-20 p-12 text-center">
+      <div className="absolute left-0 right-0 -top-14">
+        <Image
+          alt={author.name}
+          unoptimized
+          height="100px"
+          width="100px"
+          className="rounded-full align-middle"
+          src={author.photo.url}
+        />
+      </div>
+      <h3 className="my-4 text-xl font-bold text-white">{author.name}</h3>
+      <p className="text-lg text-white">{author.bio}</p>
     </div>
   );
 };
